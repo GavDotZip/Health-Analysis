3e12d8729ba7ec8cc537843031236ba7341b9235
GavDotZip <gavdotzip@gmail.com>
2024-04-06 22:54:21 +0100

    commit

--- heartAttackAnalysis/heart_analysis.py
+++ heartAttackAnalysis/heart_analysis.py
@@ -3,3 +3,7 @@ import pandas as pd
 # Load the dataset
 file_path = r'C:\Users\gavin\RProjects\Health-Analysis\heartAttackAnalysis\heart_attack.csv'
 df = pd.read_csv(file_path)
+
+# Summary statistics
+print("Summary statistics:")
+print(df.describe())
